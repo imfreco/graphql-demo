135480eacd28196dfe728be15a8fa339ede3c3de
fredycorts7 <fred.cor.14@gmail.com>
2021-06-09 11:59:11 -0500

    added custom types

--- index.js
+++ index.js
@@ -1,29 +1,20 @@
 'use strict';
 
+const { readFileSync } = require('fs');
+const { join } = require('path');
 const { buildSchema } = require('graphql');
 const express = require('express');
 const { graphqlHTTP } = require('express-graphql');
+
+const resolvers = require('./lib/resolvers');
 
 const app = express();
 const port = process.env.PORT || 3030;
 
 // define schemas
-const schema = buildSchema(`
-    type Query {
-        hello: String,
-        greeting: String
-    }
-`);
-
-// define resolvers
-const resolvers = {
-  hello: () => {
-    return 'Hello World!';
-  },
-  greeting: () => {
-    return 'Hello everybody';
-  },
-};
+const schema = buildSchema(
+  readFileSync(join(__dirname, 'lib', 'schema.graphql'), 'utf-8')
+);
 
 app.use(
   '/graphql',
