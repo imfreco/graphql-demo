96113b701a63af0027499f312125a35853f886a9
fredycorts7 <fred.cor.14@gmail.com>
2021-06-10 23:34:40 -0500

    added connection to mongodb container and adapting the queries

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 'use strict';
 
+require('dotenv').config();
+
 const { readFileSync } = require('fs');
 const { join } = require('path');
 const { makeExecutableSchema } = require('graphql-tools');
